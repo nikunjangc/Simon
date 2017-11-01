39ea79d1d00029a8e1b027d6d0722b9eeacc88b0
Amirah Oxendine <amirahoxendine@ac.c4q.nyc>
2017-10-31 20:42:38 -0400

    button drawables, and alternates with pressed states that light up

--- app/src/main/java/nyc/c4q/simon/MainActivity.java
+++ app/src/main/java/nyc/c4q/simon/MainActivity.java
@@ -2,12 +2,18 @@ package nyc.c4q.simon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button test;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //@NIKE: this is how you change the button with java
+        test = (Button) findViewById(R.id.btn_top_left);
+        test.setBackgroundResource(R.drawable.button_top_left_alt);
+
     }
 }
