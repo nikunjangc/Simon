67da580da760524678db4b710f970f6f789848ab
Amirah Oxendine <amirahoxendine@ac.c4q.nyc>
2017-10-31 20:58:34 -0400

    added timer delay runnable for lighting up the buttons

--- app/src/main/java/nyc/c4q/simon/MainActivity.java
+++ app/src/main/java/nyc/c4q/simon/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
 private Button test;
     @Override
@@ -14,6 +17,13 @@ private Button test;
         //@NIKE: this is how you change the button with java
         test = (Button) findViewById(R.id.green_btn_top_left);
         test.setBackgroundResource(R.drawable.button_top_left_alt);
+        //delay code. Put button light pattern inside the runnable
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                // this code will be executed after 2 seconds
+            }
+        }, 2000);
 
 
     }
